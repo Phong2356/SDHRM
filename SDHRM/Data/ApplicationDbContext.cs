30925f3f04589401d9e94b2dd917c096edc4000a
pdemo1 <dangphongggg@gmail.com>
2026-01-19 14:22:48 +0700

    first commit

---
+++ SDHRM/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NuGet.DependencyResolver;
+using SDHRM.Models;
+
+namespace SDHRM.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ SDHRM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SDHRM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
